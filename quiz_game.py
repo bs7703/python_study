d96e64f7a6652e94dd28dde4dda7314d7f1d836a
bs7703 <bs770311@gmail.com>
2026-08-01 22:47:13 +0900

    "make first project structure file"

---
+++ quiz_game.py
@@ -0,0 +1,24 @@
+#quiz_game.py
+
+class QuizzGame:
+	def	__init__(self):
+		pass
+
+	def	run(self):
+		pass
+
+	def	play_quiz(self):
+		pass
+
+	def	add_quiz(self):
+		pass
+
+	def	save_data(self):
+		pass
+
+	def	load_data(self):
+		pass
+
+if	__name__ == "__main__":
+	game = QuizzGame()
+	game.run()
