c2eb2068235fdcc6f70d28f7ca1e880e1c72ae87
bs7703 <bs770311@gmail.com>
2026-08-02 00:48:57 +0900

    기본적인 메뉴ui의 json화, 기본적인 메뉴의 실행함수들의 베이직뼈대 설정완료

--- quiz_game.py
+++ quiz_game.py
@@ -11,23 +11,33 @@ class QuizzGame:
 		#화면에 메뉴를 표시함
 		print(f"\n=== {self.config['game_title']} ===")
 		print(f"{self.config['ui_style']['line_char']}" * self.config['ui_style']['line_length'])
-		for	option in self.config['menu_options']['label']:	
-			print(option)
+		for	option in self.config['menu_options']:	
+			print(option['label'])
 		print(f"{self.config['ui_style']['line_char']}" * self.config['ui_style']['line_length'])
 	def	run(self):
 		while self.is_running:
-			choice = int(input("원하는 메뉴 번호를 입력하세요:").strip())
-			idx = choice - 1
+			self.display_menu()
 			try:
-				if idx >= 3 or idx <= 0:
+				choice = int(input("원하는 메뉴 번호를 입력하세요:").strip())
+				idx = choice - 1
+				if idx >= 3 or idx < 0:
 					raise IndexError
-				func = getattr(self, self.config['menu_options'][idx]['action'])
-				func()
-			except (ValueError, IndexError) :
+				func = self.config['menu_options'][idx]['action']
+				if hasattr(self, func):
+					getattr(self, func)()
+				else :
+					print("해당 함수가 정의되지 않았습니다")
+					self.is_running = False
+			except (ValueError) :
+				print("범위내 숫자 이외에 입력하지 말아주세요")
+			except (IndexError) :
 				print(f"{choice}는 적절한 번호가아닙니다 다시입력해주세요")
+			except (KeyboardInterrupt, EOFError):
+				print("사용자에의해 프로그램이 강제로종료됩니다")
+				self.is_running = False
 		pass
 
-	def	start_game(self):
+	def	start_quiz(self):
 		pass
 
 	def	show_score(self):
